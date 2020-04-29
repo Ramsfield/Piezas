1f0ac0746bd825340016db5a76f8d3635a10ce41
Ramsfield <mgriffith004@mail.csuchico.edu>
2020-04-29 11:41:03 -0700

    Added functionality to piezas. Unable to test locally due to gtest. Pushing to git to let travis-ci test

--- Piezas.cpp
+++ Piezas.cpp
@@ -13,23 +13,40 @@
  * [0,0][0,1][0,2][0,3]
  * So that a piece dropped in column 2 should take [0,2] and the next one
  * dropped in column 2 should take [1,2].
-**/
+ **/
 
 
 /**
  * Constructor sets an empty board (default 3 rows, 4 columns) and 
  * specifies it is X's turn first
-**/
+ **/
 Piezas::Piezas()
 {
+  board.resize(3); // Rows
+  for(unsigned int i = 0; i < board.size(); i++)
+  {
+    board[i].resize(4); // Columns
+    for(unsigned int j = 0; j < board[i].size(); j++)
+    {
+      board[i][j] = Blank; //Initialize all members to blank
+    }
+  }
+  turn = X;
 }
 
 /**
  * Resets each board location to the Blank Piece value, with a board of the
  * same size as previously specified
-**/
+ **/
 void Piezas::reset()
 {
+  for(unisgned int i=0; i < board.size(); i++)
+  {
+    for(unisnged int j=0; j < board.size(); j++)
+    {
+      board[i][j] = Blank; // Reset all locations to blank
+    }
+  }
 }
 
 /**
@@ -39,19 +56,43 @@ void Piezas::reset()
  * In that case, placePiece returns Piece Blank value 
  * Out of bounds coordinates return the Piece Invalid value
  * Trying to drop a piece where it cannot be placed loses the player's turn
-**/ 
+ **/ 
 Piece Piezas::dropPiece(int column)
 {
-    return Blank;
+  Piece curTurn = turn; //Save piece to put on board
+  turn = (turn == X) ? O : X; // Toggle who's turn it is
+
+  //Check out of bounds
+  if(column >= boards[0].size())
+    return Invalid;
+
+  //Check full column
+  if(boards[boards.size()-1][column] != Blank)
+    return Blank
+
+      //insert piece
+      for(unsigned int i=0; i < boards.size(); i++)
+      {
+        if(boards[i][column] == Blank)
+        {
+          boards[i][column] = curTurn;
+          return curTurn;
+        }
+      }
+  return Blank; //Should never get here, but if it did, it's an error
 }
 
 /**
  * Returns what piece is at the provided coordinates, or Blank if there
  * are no pieces there, or Invalid if the coordinates are out of bounds
-**/
+ **/
 Piece Piezas::pieceAt(int row, int column)
 {
-    return Blank;
+  //Check out of bounds, return Invalid if so
+  if((unsigned int)row >= boards.size() || (unsigned int)column >= boards[0].size())
+    return Invalid;
+
+  return boards[row][column];
 }
 
 /**
@@ -62,8 +103,68 @@ Piece Piezas::pieceAt(int row, int column)
  * the most adjacent pieces in a single line. Lines can go either vertically
  * or horizontally. If both X's and O's have the same max number of pieces in a
  * line, it is a tie.
-**/
+ **/
 Piece Piezas::gameState()
 {
+  //Holds absolute max
+  int max_x=0;
+  int max_o=0;
+
+  //Holds current calculations
+  int cur_x=0;
+  int cur_o=0;
+
+  //Check horizontal
+  for(unsigned int i=0; i < boards.size(); i++)
+  {
+    for(unsigned int j=0; j < boards[i].size(); j++)
+    {
+      //If a tile is blank, game isn't over
+      if(boards[i][j] == Blank)
+        return Invalid;
+      else if(boards[i][j] == X)
+      {
+        cur_o=0; // Reset current O's
+        cur_x++; // Update X streak
+        if(cur_x > max_x)
+          max_x = cur_x;
+      }
+      else if(boards[i][j] == O)
+      {
+        cur_x=0; // Reset current X's
+        cur_o++; // Update O streak
+        if(cur_o > max_o)
+          max_o = cur_o;
+      }
+    }
+  }
+
+  //Check Vertical
+  for(unsigned int j=0; j < boards[0].size(); j++) // Relies upon the game board being square
+  {
+    for(unsigned int i=0; i < boards.size(); i++)
+    {
+      //If a tile is blank, game isn't over
+      if(boards[i][j] == Blank)
+        return Invalid;
+      else if(boards[i][j] == X)
+      {
+        cur_o=0; // Reset current O's
+        cur_x++; // Update X streak
+        if(cur_x > max_x)
+          max_x = cur_x;
+      }
+      else if(boards[i][j] == O)
+      {
+        cur_x=0; // Reset current X's
+        cur_o++; // Update O streak
+        if(cur_o > max_o)
+          max_o = cur_o;
+      }
+    }
+  }
+  //Return winner, blank if tie
+  if(max_x == max_o)
     return Blank;
+  return (max_x > max_o) ? max_x : max_o; // Max of the two
 }
