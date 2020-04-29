d92a1a399074247b578432f9b9be6ef599cc61c3
Ramsfield <mgriffith004@mail.csuchico.edu>
2020-04-29 11:58:18 -0700

    Adding initial tests. Doing in small chunks to make sure everything goes well

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -14,7 +14,52 @@ class PiezasTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
+//SANITY CHECK
 TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+//TESTS FOR DROP PIECE
+
+// Drop an X
+TEST(PiezasTest, drop_piece_x)
+{
+  Piezas board;
+  Piece ret;
+  ret = board.dropPiece(0);
+  ASSERT_EQ(ret, X);
+}
+
+// Drop a Y
+TEST(PiezasTest, drop_piece_y)
+{
+  Piezas board;
+  Piece ret;
+  board.dropPiece(0);
+  ret = board.dropPiece(0);
+  ASSERT_EQ(ret, Y);
+}
+
+// Drop last X in column
+TEST(PiezasTest, drop_piece_last_x)
+{
+  Piezas board;
+  Piece ret;
+  board.dropPiece(0);
+  board.dropPiece(0);
+  ret = board.dropPiece(0);
+  ASSERT_EQ(ret, X);
+}
+
+// Invalid Drop
+TEST(PiezasTest, drop_piece_last_x)
+{
+  Piezas board;
+  Piece ret;
+  board.dropPiece(0);
+  board.dropPiece(0);
+  board.dropPiece(0);
+  ret = board.dropPiece(0);
+  ASSERT_EQ(ret, Invalid);
 }
